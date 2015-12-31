d8ca929b0fa4ba86064b873d34153d7e91299ca3
Anastasia Kaplina <nkaplina@gmail.com>
2015-12-30 21:59:17 -0800

    Before saving, back end checks client data for fields which are not allowed to be modified by client

--- src/server-handlers/query-handler.js
+++ src/server-handlers/query-handler.js
@@ -15,7 +15,13 @@ var Pilot = require('../orm/pilots');
 sequelize.sync();
 
 
-// must return a promise
+/**
+ *
+ * @param {string} dataType
+ * @param {object} data
+ * @param {object} pilot - sequelize instance
+ * @returns {Promise}
+ */
 function saveData(dataType, data, pilot) {
     var pilotId = pilot.getDataValue('id');
     switch (dataType) {
@@ -31,80 +37,154 @@ function saveData(dataType, data, pilot) {
 }
 
 
-// must return a promise
+/**
+ * @param {object} data
+ * @param {integer} pilotId
+ * @returns {Promise}
+ */
 function saveFlight(data, pilotId) {
+    // Make sure that we updating the information that user is allowed to update
+    if (data.see === 0) {
+        data = {
+            id: data.id,
+            see: 0
+        }
+    } else {
+        data = {
+            id: data.id,
+            date: data.date,
+            siteId: data.siteId,
+            altitude: data.altitude,
+            airtime: data.airtime,
+            gliderId: data.gliderId,
+            remarks: data.remarks
+        };
+    }
+
     if (data.id === undefined) {
         data.pilotId = pilotId;
         return Flight.create(data);
     }
 
-    if (data.id !== undefined) {
-        return Flight.findOne({ where: { id: data.id, pilotId: pilotId } }).then((flight) => {
-            if (flight === null) {
-                throw new KoiflyError(ErrorTypes.NO_EXISTENT_RECORD);
-            }
+    return Flight.findOne({ where: { id: data.id, pilotId: pilotId } }).then((flight) => {
+        if (!flight || flight.id !== data.id) {
+            throw new KoiflyError(ErrorTypes.NO_EXISTENT_RECORD);
+        }
 
-            return flight.update(data);
-        });
-    }
+        return flight.update(data);
+    });
 }
 
 
-// must return a promise
+/**
+ * @param {object} data
+ * @param {integer} pilotId
+ * @returns {Promise}
+ */
 function saveSite(data, pilotId) {
+    // Make sure that we updating the information that user is allowed to update
+    if (data.see === 0) {
+        data = {
+            id: data.id,
+            see: 0
+        }
+    } else {
+        data = {
+            id: data.id,
+            name: data.name,
+            location: data.location,
+            launchAltitude: data.launchAltitude,
+            coordinates: data.coordinates,
+            remarks: data.remarks
+        };
+    }
+
     if (data.id === undefined) {
         data.pilotId = pilotId;
         return Site.create(data);
     }
 
-    if (data.id !== undefined) {
-        return Site.findOne({ where: { id: data.id, pilotId: pilotId } }).then((site) => {
-            if (site === null) {
-                throw new KoiflyError(ErrorTypes.NO_EXISTENT_RECORD);
-            }
+    return Site.findOne({ where: { id: data.id, pilotId: pilotId } }).then((site) => {
+        if (!site || site.id !== data.id) {
+            throw new KoiflyError(ErrorTypes.NO_EXISTENT_RECORD);
+        }
 
-            // TODO transactions
-            return site.update(data);
-        }).then((newSite) => {
-            if (newSite.getDataValue('see') === false) {
-                return Flight.update({ siteId: null }, {where: {siteId: newSite.id} });
-            }
-        });
-    }
+        // TODO transactions
+        return site.update(data);
+    }).then((newSite) => {
+        if (newSite.getDataValue('see') === false) {
+            return Flight.update({ siteId: null }, {where: {siteId: newSite.id} });
+        }
+    });
 }
 
 
-// must return a promise
+/**
+ * @param {object} data
+ * @param {integer} pilotId
+ * @returns {Promise}
+ */
 function saveGlider(data, pilotId) {
+    // Make sure that we updating the information that user is allowed to update
+    if (data.see === 0) {
+        data = {
+            id: data.id,
+            see: 0
+        }
+    } else {
+        data = {
+            id: data.id,
+            name: data.name,
+            initialFlightNum: data.initialFlightNum,
+            initialAirtime: data.initialAirtime,
+            remarks: data.remarks
+        };
+    }
+
     if (data.id === undefined) {
         data.pilotId = pilotId;
         return Glider.create(data);
     }
 
-    if (data.id !== undefined) {
-        return Glider.findOne({ where: { id: data.id, pilotId: pilotId } }).then((glider) => {
-            if (glider === null) {
-                throw new KoiflyError(ErrorTypes.NO_EXISTENT_RECORD);
-            }
+    return Glider.findOne({ where: { id: data.id, pilotId: pilotId } }).then((glider) => {
+        if (!glider || glider.id !== data.id) {
+            throw new KoiflyError(ErrorTypes.NO_EXISTENT_RECORD);
+        }
 
-            // TODO transactions
-            return glider.update(data);
-        }).then((newGlider) => {
-            if (newGlider.getDataValue('see') === false) {
-                return Flight.update({ gliderId: null }, {where: {gliderId: newGlider.id} });
-            }
-        });
-    }
+        // TODO transactions
+        return glider.update(data);
+    }).then((newGlider) => {
+        if (newGlider.getDataValue('see') === false) {
+            return Flight.update({ gliderId: null }, {where: {gliderId: newGlider.id} });
+        }
+    });
 }
 
 
-// must return a promise
+/**
+ * @param {object} data - field names and new values to be updated
+ * @param {object} pilot - sequelize DB instance to update
+ * @returns {Promise.<pilot>} - pilot is sequelize DB instance
+ */
 function savePilotInfo(data, pilot) {
+    // Make sure that we updating the information that user is allowed to update
+    data = {
+        userName: data.userName,
+        initialFlightNum: data.initialFlightNum,
+        initialAirtime: data.initialAirtime,
+        altitudeUnit: data.altitudeUnit
+    };
+
     return pilot.update(data);
 }
 
 
-
+/**
+ * Saves data in case of POST request
+ * Replies with all user's data or error if the latest occurred
+ * @param {object} request
+ * @param {function} reply
+ */
 var QueryHandler = function(request, reply) {
     Pilot.findById(request.auth.credentials.userId).then((pilot) => {
         if (request.method === 'get') {
